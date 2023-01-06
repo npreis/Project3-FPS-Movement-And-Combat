199f3d6657bd29df46fe7c5cacb0c9204d085af7
npreis <nicholaspreis@gmail.com>
2023-01-06 15:47:07 -0500

    Added health bar to the enemy.

--- FPS Movement and Combat System/Assets/Scripts/EnemyHealthScript.cs
+++ FPS Movement and Combat System/Assets/Scripts/EnemyHealthScript.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealthScript : MonoBehaviour
 {
     public int health;
+    public int maxHealth;
+
     public GameObject enemy;
+    public Slider healthBar;
+
+    private void Start()
+    {
+        health = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = health;
+    }
 
     public void DecreaseHealth(int damage)
     {
@@ -17,5 +28,7 @@ public class EnemyHealthScript : MonoBehaviour
         {
             health -= damage;
         }
+
+        healthBar.value = health;
     }
 }
